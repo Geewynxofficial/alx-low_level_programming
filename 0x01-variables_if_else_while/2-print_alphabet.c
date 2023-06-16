462587ca7a0c783005b79fa271c496e32ab583b6
Geewynxofficial <godwinning247@gmail.com>
2023-06-16 16:46:23 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - startinf point of a program
+ * Return: eachtime 0 (Success)
+ */
+int main(void)
+{
+char c;
+
+c = 'a';
+while
+(c <= 'z') {
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
+

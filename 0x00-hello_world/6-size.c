24f99e2f14730a737cd03d27ad669270098acf6d
Geewynxofficial <godwinning247@gmail.com>
2023-06-15 19:47:51 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - enter a program that prints the size of various types on the computer
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %li byte(s)\n", (unsigned long)s
+return (0);
+}
+

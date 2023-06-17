fce37ad79ed317973d2db7a36cee5c84e80ebd80
Geewynxofficial <godwinning247@gmail.com>
2023-06-16 17:01:44 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - programming starting point
+ * Return: eachtime 0 (success)
+ */
+int main(void)
+{
+char c;
+
+char g;
+
+c = 'a';
+g = 'A';
+while
+(c <= 'z') {
+putchar(c);
+c++;
+}
+while
+(g <= 'z') {
+putchar (g);
+g++;
+putchar('\n');
+return (0);
+}
+

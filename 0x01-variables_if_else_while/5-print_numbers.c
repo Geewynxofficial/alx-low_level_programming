9e998bd652e83c93cd8597c1abde489317cf26fb
Geewynxofficial <godwinning247@gmail.com>
2023-06-16 18:06:12 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - entry stage of a programming
+ * Return: eachtime 0 (Success)
+ */
+
+int main(void)
+
+{
+int i;
+
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf("\n");
+
+return (0);
+
+}
